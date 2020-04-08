b2ee80ac609f1ea2b56e91cac50589459b2eaddf
n1ki4 <glazunov_n@ukr.net>
2020-04-08 12:37:43 +0300

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
 		the given email and password.
 		"""
 		user = self.create_user(email, password)
-		user.is_admin = True
+		user.is_staff = True
 		user.is_superuser = True
 		user.save(using=self._db)
 
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	)
 	name = models.CharField(max_length=255)
 	is_active = models.BooleanField(default=True)
-	is_admin = models.BooleanField(default=False)
+	is_staff = models.BooleanField(default=False)
 
 	objects = UserManager()
 
